476e1397f7e0010dda3d22ecead73922762860e9
GiaVinhNguyen <vinh.g.nguyen19@gmail.com>
2023-12-14 15:27:38 -0500

    Dodger status: dodged

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+dodger.style.left= "0px"
+
+function moveDodgerLeft() {
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "ArrowLeft") {
+            const leftNumbers = dodger.style.left.replace("px", "");
+            const left = parseInt(leftNumbers, 10);
+            if (left > 0) {
+                dodger.style.left = `${left - 10}px`;}
+        };
+    });    
+};
+
+function moveDodgerRight() {
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "ArrowRight") {
+            const leftNumbers = dodger.style.left.replace("px", "");
+            const left = parseInt(leftNumbers, 10);
+            if (left < 360) {
+                dodger.style.left = `${left + 10}px`;};
+        };
+    });
+}
+
+moveDodgerLeft();
+moveDodgerRight();
